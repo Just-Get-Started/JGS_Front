5f4ab38ec269a0bb1c27aeeaee9467e12ef2c246
King <tmdwns563@naver.com>
2024-08-12 22:32:28 +0900

    구글 로그인 진행중

--- src/Sign/Login.js
+++ src/Sign/Login.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-
+import { useState, useEffect } from 'react';
 const Login = () => {
 
+    // const userInfo = sessionStorage.getItem(Access_Token);
+    // console.log(userInfo);
     //네이버 로그인
     const onNaverLogin = () => {
-        window.location.href = "http://localhost:8080/oauth2/authorization/naver";
-      };
+      window.location.href = "http://localhost:8080/oauth2/authorization/naver"
+    };
     //구글 로그인
     const onGoogleLogin = () => {
-        window.location.href = "http://localhost:8080/oauth2/authorization/google";
-      };
+      window.location.href = "http://localhost:8080/oauth2/authorization/google";
+    }
+
+    useEffect(() => {
+      axios.get('http://localhost:8080/oauth2/token')
+      .then(res => {
+          const accessToken = res.data.Access_Token;
+          console.log(res);
+          console.log(accessToken);
+          sessionStorage.setItem('Access_Token', accessToken);
+      }).catch(err => {
+        console.log(err);
+      })
+},[]);
+
+
     //카카오 로그인
       const onKakaoLogin = () => {
         window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
