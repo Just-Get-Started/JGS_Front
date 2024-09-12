f3b1ae75f7129c6c64ad4766c9893a1808848b67
King <tmdwns563@naver.com>
2024-09-13 00:49:38 +0900

    매치등록 카카오맵띄우기

--- src/Pages/Match.js
+++ src/Pages/Match.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Match = () => {
     const [searchType, setSearchType] = useState('keyword'); // 검색 기준 (팀명 또는 티어)
     const [searchValue, setSearchValue] = useState('');
     const [matchInfoList, setMatchInfoList] = useState([]);
-    const [selectedMatch, setSelectedMatch] = useState([]);
+    const [selectedMatch, setSelectedMatch] = useState(null);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [last, setLast] = useState(false);
+    const [isOpenModal, setIsOpenModal] = useState(false);  //
+    const [isEditModal, setIsEditModal] = useState(false);  //수정 토글
+    const [editedMatch, setEditedMatch] = useState({
+        matchDate: '',
+        location: ''
+    });
 
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get(`http://localhost:8080/match-post`, {
             params: {
-                [searchType]: searchValue, // 검색 기준에 따라 keyword 또는 tier를 쿼리 파라미터로 사용
+                [searchType]: searchValue, // 검색 기준에 따라 keyword 또는 tier
                 page: page-1
             }
         }).then((res) => {
-
             setMatchInfoList(res.data.matchPostDTOList);
             setTotalPages(res.data.totalPages);
             setLast(res.data.last);
@@ -30,8 +37,15 @@ const Match = () => {
 
     const handleMatchClick = (match) => {
         setSelectedMatch(match);
+        setIsOpenModal(true);
     };
 
+    const handleCloseModal = () => {
+        setIsOpenModal(false);
+        setSelectedMatch(null);
+    }
+    
+    
     const handleSearchTypeChange = (e) => {
         setSearchType(e.target.value);
     };
@@ -40,16 +54,84 @@ const Match = () => {
         setSearchValue(e.target.value);
     };
 
+    //이전 페이지네이션
     const handlePrevPage = () => {
         if (page > 1) {
             setPage(page - 1);
         }
     };
 
+    //다음 페이지네이션
     const handleNextPage = () => {
         if (!last) {
             setPage(page + 1);
         }
+    };
+
+    //매치 등록버튼
+    const handleRegisterClick = () => {
+        navigate("/matchregister");
+    }
+
+    // 매치 글 삭제
+    const handleDeleteClick = (match) => {
+        axios.delete(`http://localhost:8080/api/match-post`, {
+            params: {
+                matchPostId: match.matchPostId
+            },
+            headers: {
+                'Access_Token': localStorage.getItem('Access_Token')
+            }
+        }).then((res) => {
+            console.log(res.data);
+            setMatchInfoList(prevList => prevList.filter(item => item.matchPostId !== match.matchPostId));
+            handleCloseModal(); // 모달 닫기
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+
+    // 수정버튼
+    const editButton = () => {
+        setIsEditModal(true);
+        setEditedMatch({
+            matchDate: selectedMatch.matchDate,
+            location: selectedMatch.location
+        });
+    };
+
+    // 수정 사항 입력
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+        setEditedMatch((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    // 수정사항 저장
+    const saveChanges = () => {
+        axios.put(`http://localhost:8080/api/match-post`, {
+                matchPostId: selectedMatch.matchPostId,
+                matchDate: editedMatch.matchDate,
+                location: editedMatch.location
+            }, 
+            {
+                headers: {
+                'Access_Token' : localStorage.getItem('Access_Token')
+            }
+        }).then((res) => {
+            setIsOpenModal(false);
+            setIsEditModal(false);
+            setMatchInfoList((prevList) => 
+            prevList.map    ((item) => 
+            item.matchPostId === selectedMatch.matchPostId
+        ? {...item, ...editedMatch}
+            :item
+        ))
+        }).catch((err) => {
+            console.log(err);
+        });
     };
 
     return (
@@ -72,7 +154,7 @@ const Match = () => {
                         matchInfoList.map((match) => (
                             <MatchLineContainer key={match.matchPostId} onClick={() => handleMatchClick(match)}>
                                 <MatchTeamName>팀명: {match.teamName}</MatchTeamName>
-                                <MatchTier>티어: {match.tier || '정보 없음'}</MatchTier>
+                                <MatchTier>티어: {match.tierName || '정보 없음'}</MatchTier>
                                 <MatchLocation>구장명: {match.location}</MatchLocation>
                                 <MatchDate>시간: {new Date(match.matchDate).toLocaleString()}</MatchDate>
                             </MatchLineContainer>
@@ -90,7 +172,53 @@ const Match = () => {
                 <PaginationButton onClick={handleNextPage} disabled={last}>
                     다음
                 </PaginationButton>
+                <RegisterButton onClick={handleRegisterClick}>매치 등록</RegisterButton>
             </PaginationContainer>
+            {isOpenModal && selectedMatch && (
+    <ModalOverlay>
+        <ModalContainer>
+            <CloseButton onClick={handleCloseModal}>X</CloseButton>
+            <ModalContent>
+                <h2>{selectedMatch.teamName}</h2>
+                {isEditModal ? (
+                    <>
+<p><strong>경기 날짜:</strong>
+    <input
+        type="datetime-local"
+        name="matchDate"
+        value={new Date(new Date(editedMatch.matchDate).getTime() - new Date().getTimezoneOffset() * 60000)
+            .toISOString().slice(0, 16)} // 로컬 시간대로 변환
+        onChange={handleInputChange}
+    />
+</p>
+
+                        <p><strong>구장명:</strong>
+                            <input
+                                type="text"
+                                name="location"
+                                value={editedMatch.location}
+                                onChange={handleInputChange}
+                            />
+                        </p>
+                        <ButtonContainer>
+                            <EditButton onClick={saveChanges}>저장</EditButton>
+                            <DeleteButton onClick={handleCloseModal}>취소</DeleteButton>
+                        </ButtonContainer>
+                    </>
+                ) : (
+                    <>
+                        <p><strong>경기 날짜:</strong> {new Date(selectedMatch.matchDate).toLocaleString()}</p>
+                        <p><strong>구장명:</strong> {selectedMatch.location}</p>
+                        <ButtonContainer>
+                            <EditButton onClick={editButton}>수정</EditButton>
+                            <DeleteButton onClick={() => handleDeleteClick(selectedMatch)}>삭제</DeleteButton>
+                        </ButtonContainer>
+                    </>
+                )}
+            </ModalContent>
+        </ModalContainer>
+    </ModalOverlay>
+)}
         </MainContainer>
     );
 };
@@ -206,4 +334,109 @@ const PaginationText = styled.span`
     margin: 0 15px;
 `;
 
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalContainer = styled.div`
+  background: white;
+  padding: 2rem;
+  border-radius: 10px;
+  width: 400px;
+  position: relative;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: none;
+  border: none;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+const ModalContent = styled.div`
+  text-align: center;
+  h2 {
+    margin-bottom: 1rem;
+  }
+  p {
+    margin: 0.5rem 0;
+  }
+`;
+
+const RegisterButton = styled.button`
+    padding: 10px 20px;
+    font-size: 16px;
+    margin-left: 20px;
+    border: none;
+    border-radius: 5px;
+    background-color: #28a745;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: #218838;
+    }
+
+    &:active {
+        background-color: #1e7e34;
+    }
+`
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+`;
+
+const EditButton = styled.button`
+    padding: 10px 20px;
+    background-color: #ffc107;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: #e0a800;
+    }
+
+    &:active {
+        background-color: #d39e00;
+    }
+`;
+
+const DeleteButton = styled.button`
+    padding: 10px 20px;
+    background-color: #dc3545;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: #c82333;
+    }
+
+    &:active {
+        background-color: #bd2130;
+    }
+`;
 export default Match;
--- src/Pages/MatchRegisterPage.js
+++ src/Pages/MatchRegisterPage.js
@@ -1,9 +1,69 @@
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import { Markerdata } from "../Components/MarkerData";
+
+const {kakao} = window;
 
 const MatchRegisterPage = () => {
+    useEffect(() => {
+        mapscript();
+    }, []);
+
+    const mapscript = () => {
+        let container = document.getElementById("map");
+        let options = {
+            center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
+            level: 5,
+        };
+
+        // 지도 생성
+        const map = new kakao.maps.Map(container, options);
+
+        Markerdata.forEach((el) => {
+            const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; // 원하는 이미지 URL로 변경
+            const imageSize = new kakao.maps.Size(24, 35); // 이미지 크기
+
+            // 마커 이미지 객체 생성
+            const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+            // 마커 생성
+            const marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(el.lat, el.lng),
+                title: el.title,
+                image: markerImage, // 커스텀 마커 이미지 적용
+            });
+
+            // 정보 창 생성
+            const infowindow = new kakao.maps.InfoWindow({
+                content: `<div style="padding:5px;">${el.title}</div>`,
+                removable: true
+            });
+
+            // 마커 클릭 시 정보 창 표시
+            kakao.maps.event.addListener(marker, 'click', () => {
+                infowindow.open(map, marker);
+            });
+        });
+    };
+
+    const handleRegisterClick = () => {
+        axios.post(`http://localhost:8080/api/match-post`, {
+            data: {
+                teamName: 'ㄴㅇ'
+            }
+        });
+    };
+
     return (
-        <>
-        </>
-    )
-}
+        <div>
+            <div id="map" style={{
+                width: '1200px',
+                height: '600px'
+            }}></div>
+        </div>
+    );
+};
+
 export default MatchRegisterPage;
